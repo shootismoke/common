43012a1505e11a7615e92bda1bc54acdc7733ad9
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-05 00:42:25 +0100

    chore: Update types to latest versions (#25)

--- packages/graphql/.eslintrc.js
+++ packages/graphql/.eslintrc.js
@@ -2,6 +2,9 @@ const base = require('../../.eslintrc');
 
 module.exports = {
   ...base,
+  parserOptions: {
+    project: './tsconfig.settings.json'
+  },
   rules: {
     // Generated code from graphql-codegen has some any types
     '@typescript-eslint/no-explicit-any': 'off'
