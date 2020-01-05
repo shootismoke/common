44890d8de0d89b1fe346dcb4ebb2087fb3e690d0
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-05 12:51:51 +0100

    Add country for normalized aqicn

--- packages/dataproviders/src/providers/aqicn/normalize.ts
+++ packages/dataproviders/src/providers/aqicn/normalize.ts
@@ -1,7 +1,9 @@
 import { convert, getMetadata, Pollutant, usaEpa } from '@shootismoke/convert';
 import * as E from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/pipeable';
 
 import { Normalized } from '../../types';
+import { getCountryCode } from '../../util';
 import { ByStation } from './validation';
 
 /**
@@ -37,41 +39,55 @@ export function normalize(data: ByStation): E.Either<Error, Normalized> {
       )
     );
   }
+  // We now need to get the country from AQICN response. The only place I found
+  // is city.url...
+  if (!data.city.url || !data.city.url.startsWith('http://aqicn.org/city/')) {
+    return E.left(
+      new Error(`Cannot extract country, got city.url: ${data.city.url}`)
+    );
+  }
+  const [countryRaw] = data.city.url.split('/');
+  // FIXME Should we use an external service to get country from lat/lng?
 
-  return E.right(
-    pollutants.map(([pol, { v }]) => {
-      const pollutant = pol as Pollutant;
+  return pipe(
+    getCountryCode(countryRaw),
+    E.fromOption(() => new Error(`Cannot get code from country ${countryRaw}`)),
+    E.map(
+      country =>
+        pollutants.map(([pol, { v }]) => {
+          const pollutant = pol as Pollutant;
 
-      if (!data.city.geo) {
-        throw new Error(
-          'We returned TE.left if data.city.geo was not defined. qed.'
-        );
-      }
+          if (!data.city.geo) {
+            throw new Error(
+              'We returned TE.left if data.city.geo was not defined. qed.'
+            );
+          }
 
-      return {
-        attribution: data.attributions,
-        averagingPeriod: {
-          unit: 'day',
-          value: 1
-        },
-        city: data.city.name || 'Unknown city', // FIXME Don't put "unknown" here
-        coordinates: {
-          latitude: +data.city.geo[0],
-          longitude: +data.city.geo[1]
-        },
-        country: 'Unknown country', // FIXME Don't put "unknown" here
-        date: {
-          local: new Date(+data.time.v * 1000).toISOString(),
-          utc: new Date(+data.time.v * 1000).toUTCString() // FIXME Not 100% sure this is correct
-        },
-        location: stationId,
-        mobile: false,
-        parameter: data.dominentpol as Pollutant,
-        sourceName: 'aqicn',
-        sourceType: 'other',
-        value: convert(pollutant, 'usaEpa', 'raw', v),
-        unit: getMetadata(pollutant).preferredUnit
-      };
-    }) as Normalized
+          return {
+            attribution: data.attributions,
+            averagingPeriod: {
+              unit: 'day',
+              value: 1
+            },
+            city: data.city.name || 'Unknown city', // FIXME Don't put "unknown" here
+            coordinates: {
+              latitude: +data.city.geo[0],
+              longitude: +data.city.geo[1]
+            },
+            country,
+            date: {
+              local: new Date(+data.time.v * 1000).toISOString(),
+              utc: new Date(+data.time.v * 1000).toUTCString() // FIXME Not 100% sure this is correct
+            },
+            location: stationId,
+            mobile: false,
+            parameter: data.dominentpol as Pollutant,
+            sourceName: 'aqicn',
+            sourceType: 'other',
+            value: convert(pollutant, 'usaEpa', 'raw', v),
+            unit: getMetadata(pollutant).preferredUnit
+          };
+        }) as Normalized
+    )
   );
 }
