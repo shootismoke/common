39d499b12cd91cd66d7f0295d53e3701d2bea794
Amaury <amaury.martiny@protonmail.com>
2021-01-14 19:57:01 +0100

    fix: Rename raw to ugm3 in convert, better UI utils (#387)
    * fix: Rename raw to ugm3 in convert, better UI utils
    
    * promise test in e2e

--- packages/dataproviders/test/e2e/waqi.spec.ts
+++ packages/dataproviders/test/e2e/waqi.spec.ts
@@ -2,7 +2,11 @@ import { waqi } from '../../src/providers/waqi';
 import { testProviderE2E } from '../../src/util';
 
 describe('waqi e2e', () => {
+	beforeAll(() => jest.setTimeout(30000));
+
 	testProviderE2E(waqi, {
 		skip: ['fetchByStation'],
 	});
+
+	afterAll(() => jest.setTimeout(5000));
 });
